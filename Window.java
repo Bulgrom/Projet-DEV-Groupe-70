f670001ba721ead219692eeb8ab95cd33e6d78f8
BusterJava <joris.thaveau@gmail.com>
2017-04-08 11:53:52 +0200

    Add files via upload

--- Window.java
+++ Window.java
@@ -34,8 +34,7 @@ public class Window extends JFrame {
 		JButton newGame = new JButton("New Game");
 		JButton levelSelect = new JButton("Select a level");
 
-		Background[] backgrounds = {new Background("forest.jpg"), 
-				new Background("longforest.jpg"), new Background ("darkforest.jpg")};
+		Background[] backgrounds = {new Background("forest.jpg"), new Background("longforest.jpg"), new Background ("darkforest.jpg")};
 		
 		resume.setMaximumSize(new Dimension(500,40));
 		newGame.setMaximumSize(new Dimension(500,40));
@@ -46,24 +45,20 @@ public class Window extends JFrame {
 		this.setMinimumSize(new Dimension(600,438));
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		height = this.getHeight();
-		width = this.getWidth();
-		System.out.print(width);
+		
 		
 		while(true){
 
-			
 			Background currentBackground = backgrounds[i%3];
 			currentBackground.setLayout(new BoxLayout(currentBackground,BoxLayout.PAGE_AXIS));
 
 			currentBackground.add(Box.createVerticalGlue());
 			currentBackground.add(resume);
 			currentBackground.add(newGame);
-			currentBackground.add(Box.createRigidArea(new Dimension((width/2)-250,10)));
+			currentBackground.add(Box.createRigidArea(new Dimension(60,10)));
 			currentBackground.add(levelSelect);
 			currentBackground.add(Box.createVerticalGlue());
 			this.setContentPane(currentBackground);;
-
 			this.setVisible(true);
 			Thread.sleep(7000);
 			i++;
