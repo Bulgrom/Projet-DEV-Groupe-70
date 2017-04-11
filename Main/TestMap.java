619ffeda459dcc0d3ecfe24318aa79610f7b3c60
jdesmazi <juliendesmaze@gmail.com>
2017-04-11 22:51:45 +0200

    Add files via upload

--- Main/TestMap.java
+++ Main/TestMap.java
@@ -1,19 +1,20 @@
 package Main;
 
 import java.io.IOException;
+import java.io.File;
 
 public class TestMap {
 
 	public static void main(String[] args) throws IOException {
 		// Test a refaire il ne fonctionne plus
-		Map mapTest = new Map("C:/Users/Julien/Desktop/Map/initial/test.txt"); 
-		mapTest.setName("Save");
-		System.out.println("Nom change");
-		mapTest.setType("instance");
-		System.out.println("Type change");
-		mapTest.saveMap("C:/Users/Julien/Desktop/Map/initial/");
-		System.out.println("Sauvegarde execute");
-		
+		//Map mapTest = new Map("C:/Users/Julien/Desktop/Map/initial/test.txt"); 
+		//mapTest.setName("Save");
+		//System.out.println("Nom change");
+		//mapTest.setType("instance");
+		//System.out.println("Type change");
+		//mapTest.saveMap("C:/Users/Julien/Desktop/Map/initial/");
+		//System.out.println("Sauvegarde execute");
+		new File("Directory1").mkdir();
 	}
 
 }
