2f04a683eed7e749264b87a9c531f872e50ed76b
jdesmazi <juliendesmaze@gmail.com>
2017-06-01 12:13:46 +0200

    Add files via upload

---
+++ Map.java
@@ -0,0 +1,165 @@
+package map;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import element.Element;
+import ability.trap.Trap;
+
+
+public class Map {
+	private static String backSpace = System.lineSeparator();
+	private String name;
+	private Square[][] map;
+	protected int[] dim = new int[2];
+	
+	
+	public Map(String directory) throws IOException, InstantiationException, IllegalAccessException, ClassNotFoundException{
+	    String[][] stringMap = readMap(new File(directory + "Map.txt"));
+	    
+	    setMap(new Square[dim[0]][dim[1]]);
+	    for(int i=0; i<dim[0]; i++){
+	    	for(int j=0; j<dim[1]; j++){
+	    		setSquare(i,j,new Square(stringMap[i][j], i, j, directory, this));
+	    	}
+	    }
+	   }
+	
+	public Map(String name, int height, int length){
+		map = new Square[height][length];
+		setDim(height, length);
+		for(int i=0; i<height; i++){
+			for(int j=0; j<length; j++){
+				map[i][j] = new Square(i, j, this);
+				map[i][j].setBackground(Background.GRASS);
+			}
+		}
+	}
+			
+	public String getName(){
+		return name;
+	}
+	
+	public Square[][] getMap(){
+		return map;
+	}
+	
+	public Square getSquare(int n, int m){
+		return map[n][m];
+	}
+	
+
+	public int getLines(){
+		return dim[0];
+	}
+	
+	public int getColumns(){
+		return dim[1];
+	}
+	
+	public int[] getDim(){
+		return dim;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setMap(Square[][] map){
+		this.map = map;
+	}
+	
+	public void setSquare(int i, int j, Square square){
+		map[i][j] = square;
+	}
+	
+	public void setDim(int i, int j){
+		dim[0] = i;
+		dim[1] = j;
+	}
+	
+	public String[][] readMap(File root) throws IOException{
+		BufferedReader reader = new BufferedReader(new FileReader(root));
+		setName(reader.readLine());
+		String[] stringDim = reader.readLine().split("x");
+		this.dim[0] = Integer.parseInt(stringDim[0]);
+		this.dim[1] = Integer.parseInt(stringDim[1]);
+		
+		String[][] stringMap = new String[dim[0]][dim[1]];
+		for (int i = 0; i < dim[0]; i++) {
+			stringMap[i] = reader.readLine().split("_");
+		}
+		reader.close();
+		return stringMap;
+		
+	}
+
+	public void saveMap(String directory){
+	    try {
+			new File(directory).mkdirs();
+	    	File file = new File(directory + "Map.txt");
+	        file .createNewFile();
+	        (new File(directory + "Element.txt")).createNewFile();
+	        (new File(directory + "Trap.txt")).createNewFile();
+	        final FileWriter writer = new FileWriter(file);
+	        
+	        LinkedList<Element> elementList = new LinkedList<Element>();
+	        LinkedList<Trap> trapList = new LinkedList<Trap>();
+	        
+	        final FileWriter elementWriter = new FileWriter(new File(directory + "Element.txt"));
+	        final FileWriter trapWriter = new FileWriter(new File(directory + "Trap.txt"));
+	        try {
+	        	writer.write(name + backSpace + dim[0] + "x" + dim[1] + backSpace);
+	        	for (int i=0; i < dim[0]; i++){
+	        		for (int j=0; j < dim[1]; j++){
+	        			writer.write(getSquare(i, j) + "_");
+	        			if((getSquare(i,j).getElement() != null) && !(elementList.contains(getSquare(i,j).getElement()))) 
+	        					elementList.add(getSquare(i,j).getElement());
+	        			if((getSquare(i,j).getTrap() != null) && !(trapList.contains(getSquare(i,j).getTrap()))) 
+        						trapList.add(getSquare(i,j).getTrap());
+	        		}
+	        		writer.write(backSpace);
+	        	}
+	        	
+	        	for(int i=0; i<elementList.size();i++){
+	        		elementWriter.write(elementList.get(i).getCodex() + backSpace);
+	        	}
+	        	
+	        	for(int i=0; i<trapList.size(); i++){
+	        		trapWriter.write(trapList.get(i).getCodex() + backSpace);
+	        	}
+	        }
+	        finally {
+	            writer.close();
+	            elementWriter.close();
+	            trapWriter.close();
+	        }
+	    }
+	    catch (Exception e) {
+	        System.out.println("Impossible de creer le fichier Map " + name);
+	    }
+	}
+
+	public String toString(){
+		String str = "";
+		for(int i=0; i<dim[0]; i++){
+	    	for(int j=0; j<dim[1]; j++){
+	    		str += getSquare(i,j) + " ";
+	    	}
+	    	str += backSpace;
+	    }
+		return str;
+	}
+	
+	public boolean equals(Object map){
+		if(!(map instanceof Map)) return false;
+		
+		Map m = (Map) map;
+		
+		return (m.toString().equals(this.toString())) && (m.getName().equals(name));
+	}
+
+}
---
+++ MapTest.java
@@ -0,0 +1,210 @@
+package map ;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import ability.*;
+import ability.damageAbility.*;
+import element.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MapTest {
+
+	private final static String backSpace = System.lineSeparator();
+	private Map m;
+	private Character pers1;
+	private Character pers2;
+	private Ability[] atk;
+	
+	@Before
+	public void before(){
+		System.out.println(backSpace + "Nouveau Test");
+		m = new Map("Test",4,5);
+		System.out.println(m);
+		
+		pers1 = new Character("pers1", 1, 1000, 3, 6, 20, 30, 10);
+		pers2 = new Character("pers2", 2, 1000, 5, 6, 0, 100, 20);
+		System.out.println(pers1);
+		System.out.println(pers2);
+		
+		atk = new Ability[2];
+		atk[0] = new Punch(50);
+		atk[1] = new BasicArrow(50, 4);
+		pers1.setAbilities(atk);
+		//pers2.setAbilities(atk);
+		
+		System.out.println("Fin du before" + backSpace);
+	}
+	
+	@Test
+	public void testPunch() {
+		System.out.println("testPunch");
+		
+		m.getSquare(0,0).setCharacter(pers1);
+		m.getSquare(0,1).setCharacter(pers2);
+		
+		System.out.println(m);
+		System.out.println("");
+		
+		
+		System.out.println(pers2.getPV());
+		assertTrue(pers2.getPV() == 1000);
+		
+		pers1.useAbility(pers1.getAbility(0), m.getSquare(0,1));
+		assertTrue(pers2.getPV() == 950);
+		System.out.println(pers2.getPV());
+		
+		pers1.useAbility(new Punch(50), m.getSquare(0,1));
+		assertTrue(pers2.getPV() == 950);
+		System.out.println(pers2.getPV());
+		
+		pers1.useAbility(pers1.getAbility(0), m.getSquare(0,2));
+		assertTrue(pers2.getPV() == 950);
+		System.out.println(pers2.getPV());
+		
+		pers1.useAbility(pers1.getAbility(0), m.getSquare(1,0));
+		assertTrue(pers2.getPV() == 950);
+		System.out.println(pers2.getPV());
+		
+	}
+	
+	@Test
+	public void testBasicArrow(){
+		System.out.println("testBasicArrow");
+		
+		m.getSquare(0, 0).setCharacter(pers2);
+		m.getSquare(0, 1).setCharacter(pers2);
+		m.getSquare(1, 3).setCharacter(pers2);
+		m.getSquare(2, 1).setCharacter(pers2);
+		m.getSquare(2, 2).setCharacter(pers2);
+		m.getSquare(3, 2).setCharacter(pers2);
+		m.getSquare(3, 4).setCharacter(pers2);
+		m.getSquare(1, 4).setCharacter(pers1);
+		m.getSquare(2, 3).setElement(new Wall());
+		m.getSquare(2, 4).setElement(new SmallRock());
+		
+		System.out.println(m);
+		System.out.println("");
+		
+		
+		System.out.println(pers2.getPV());
+		assertTrue(pers2.getPV() == 1000);
+		
+		pers1.useAbility(pers1.getAbility(1), m.getSquare(0,0));
+		assertTrue(pers2.getPV() == 1000);
+		System.out.println(pers2.getPV());
+		
+		pers1.useAbility(pers1.getAbility(1), m.getSquare(0,1));
+		assertTrue(pers2.getPV() == 950);
+		System.out.println(pers2.getPV());
+		
+		pers1.useAbility(pers1.getAbility(1), m.getSquare(1,3));
+		assertTrue(pers2.getPV() == 900);
+		System.out.println(pers2.getPV());
+		
+		pers1.useAbility(pers1.getAbility(1), m.getSquare(2,1));
+		assertTrue(pers2.getPV() == 900);
+		System.out.println(pers2.getPV());
+		
+		pers1.useAbility(pers1.getAbility(1), m.getSquare(2,2));
+		assertTrue(pers2.getPV() == 900);
+		System.out.println(pers2.getPV());
+		
+		pers1.useAbility(pers1.getAbility(1), m.getSquare(3,2));
+		assertTrue(pers2.getPV() == 900);
+		System.out.println(pers2.getPV());
+		
+		pers1.useAbility(pers1.getAbility(1), m.getSquare(3,4));
+		assertTrue(pers2.getPV() == 850);
+		System.out.println(pers2.getPV());
+		
+	}
+	
+	@Test
+	public void testSaveMap() throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException{
+		System.out.println("TestSaveMap");
+		
+		m.getSquare(0,1).setBackground(Background.SAND);
+		m.getSquare(0, 2).setElement(new Wall());
+		m.getSquare(0, 2).getElement().setId(1);
+		
+		m.getSquare(1,0).setBackground(Background.WATER);
+		m.getSquare(1,0).setElement(new SmallRock());
+		m.getSquare(1,0).getElement().setId(2);
+		System.out.println(m);
+		
+		m.saveMap("mapSave/Test/");
+		System.out.println("Map sauvegarde");
+		
+		Map m2 = new Map("mapSave/Test/");
+		System.out.println("Map Chargee" + backSpace);
+		
+		assertTrue(m.getName().equals(m2.getName()));
+		
+		System.out.println(m2);
+		assertTrue(m2.getSquare(0,0).equals(m.getSquare(0,0)));
+		
+		assertTrue(m2.equals(m));
+
+	}
+
+	
+	@Test
+	public void testSaveCharacter() throws InstantiationException, IllegalAccessException, ClassNotFoundException, IOException{
+		System.out.println("TestSaveCharacter");
+		
+		pers1.saveCharacterWithName("characterSave/");
+		System.out.println("pers1 sauvegarde avec nom");
+		
+		pers2.saveCharacterWithName("characterSave/");
+		System.out.println("pers2 sauvegarde avec nom");
+		
+		
+		pers2.setStatus(Status.DEAD);
+		pers2.saveCharacterWithId("characterSave/");
+		System.out.println("pers2 sauvegarde avec Id");
+		
+		Character c = new Character("characterSave/pers1.txt");
+		System.out.println("Character Chargee");
+		
+		assertTrue(c.equals(pers1));
+		
+		Character c2 = new Character("characterSave/pers2.txt");
+		System.out.println("Character Chargee");
+		
+		assertTrue(c2.equals(pers2));
+	}
+	
+
+	@Test
+	public void testMoveCharacter(){
+		System.out.println("testMoveCharacter");
+		
+		m.getSquare(0, 0).setCharacter(pers1);
+		m.getSquare(3, 4).setCharacter(pers2);
+		System.out.println(m);
+		
+		assertFalse(pers1.checkMove(m.getSquare(0, 4)));
+		pers1.move(m.getSquare(0, 4));
+		System.out.println(m);
+		
+		
+		assertTrue(pers1.checkMove(m.getSquare(1, 2)));
+		pers1.move(m.getSquare(1, 2));
+		System.out.println(m);
+		
+		pers1.move(m.getSquare(3, 2));
+		
+		assertFalse(pers1.checkMove(m.getSquare(3, 4)));
+		pers1.move(m.getSquare(3, 4));
+		
+		System.out.println(m);
+		
+		
+		
+	}
+	
+	
+}
